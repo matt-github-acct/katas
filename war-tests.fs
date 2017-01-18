a1eb579eef9c6f3b0d77ec9280fdae5b24652426
Matt Baker <baker.mattc@gmail.com>
2017-01-18 13:34:30 -0700

    Create war-tests.fs

---
+++ war-tests.fs
@@ -0,0 +1,42 @@
+module war_tests
+
+open NUnit.Framework
+
+[<Test>]
+let ``the highest rank wins the cards in the round`` () = 
+    Assert.Fail "TODO"
+
+[<Test>]
+[<Ignore"">]
+let ``queens are higher rank than jack`` () = 
+    Assert.Fail "TODO"
+
+[<Test>]
+[<Ignore"">]
+let ``kings are higher rank than queens`` () = 
+    Assert.Fail "TODO"
+
+[<Test>]
+[<Ignore"">]
+let ``aces are higher rank than kings`` () = 
+    Assert.Fail "TODO"
+
+[<Test>]
+[<Ignore"">]
+let ``if the ranks are equal, clubs beat spades`` () = 
+    Assert.Fail "TODO"
+
+[<Test>]
+[<Ignore"">]
+let ``if the ranks are equal, diamonds beat clubs`` () = 
+    Assert.Fail "TODO"
+
+[<Test>]
+[<Ignore"">]
+let ``if the ranks are equal, hearts beat diamonds`` () = 
+    Assert.Fail "TODO"
+
+[<Test>]
+[<Ignore"">]
+let ``the player loses when they run out of cards`` () = 
+    Assert.Fail "TODO"
