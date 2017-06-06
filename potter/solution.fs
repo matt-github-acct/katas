650a818f8945d306b362a77342e94ec598a13194
Matt Baker <baker.mattc@gmail.com>
2017-06-06 17:13:26 -0600

    (potter) solution WIP

--- potter/solution.fs
+++ potter/solution.fs
@@ -2,14 +2,28 @@ module Tests
 
 open Xunit
 
-type Book = First | Second | Third | Fourth
+type Book = First | Second | Third | Fourth | Fifth
+
+let distinctTitleCount books = 
+    List.distinct books |> List.length
+
+let calculateCartDiscount books = 
+    match distinctTitleCount books with
+    | 5 -> 0.75
+    | 4 -> 0.8 
+    | 3 -> 0.90 
+    | 2 -> 0.95
+    | _ -> 1.
+
+let round (number:float) = 
+    System.Math.Round (number, 2)
 
 let calculateCartCost books =
-    match List.distinct books |> List.length with
-    | 4 -> 8. - (8. * 0.2)
-    | 3 -> 8. - (8. * 0.1)
-    | 2 -> 8. - (8. * 0.05)
-    | _ -> 8.
+    books
+    |> calculateCartDiscount
+    |> (*) (float (List.length books))
+    |> (*) 8.
+    |> round
 
 [<Fact>]
 let ``should return 8 for a single book`` () =
@@ -17,16 +31,24 @@ let ``should return 8 for a single book`` () =
 
 [<Fact>]
 let ``should return 8 for a multiples of the same book`` () =
-     Assert.Equal(8., calculateCartCost [Book.First; Book.First])
+     Assert.Equal(16., calculateCartCost [Book.First; Book.First])
 
 [<Fact>]
-let ``should return 8 with 5% discount for two unique books`` () =
-     Assert.Equal(7.6, calculateCartCost [Book.First; Book.Second])
+let ``should return 16 with 5% discount for two unique books`` () =
+     Assert.Equal(15.2, calculateCartCost [Book.First; Book.Second])
 
 [<Fact>]
-let ``should return 8 with 10% discount for three unique books`` () =
-     Assert.Equal(7.2, calculateCartCost [Book.First; Book.Second; Book.Third])
+let ``should return 24 with 10% discount for three unique books`` () =
+     Assert.Equal(21.6, calculateCartCost [Book.First; Book.Second; Book.Third])
 
 [<Fact>]
-let ``should return 8 with 20% discount for four unique books`` () =
-     Assert.Equal(6.4, calculateCartCost [Book.First; Book.Second; Book.Third; Book.Fourth])
+let ``should return 32 with 20% discount for four unique books`` () =
+     Assert.Equal(25.6, calculateCartCost [Book.First; Book.Second; Book.Third; Book.Fourth])
+
+[<Fact>]
+let ``should return 40 with 25% discount for four unique books`` () =
+     Assert.Equal(30., calculateCartCost [Book.First; Book.Second; Book.Third; Book.Fourth; Book.Fifth])
+
+[<Fact>]
+let ``should return 24 with a 5% discount for a multiples of the same book with one unique pair`` () =
+     Assert.Equal(22.8, calculateCartCost [Book.First; Book.First; Book.Second])
